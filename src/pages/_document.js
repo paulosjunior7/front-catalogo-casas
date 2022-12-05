48100029d4eddd7036edce58b01251782d350dfd
Paulo Soares <paulosjunior7@gmail.com>
2022-12-04 23:17:50 -0300

    ajuste documento

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import Page from '../../components/Page';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
-      {/* <Page title="Inicio" description="teste"> */}
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      {/* </Page> */}
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
     </Html>
   )
 }
