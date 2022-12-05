c3c58efbec839896e62b8792832762c71224eeda
Paulo Soares <paulosjunior7@gmail.com>
2022-12-04 22:59:04 -0300

    ajust docment

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,17 @@ import Page from '../../components/Page';
 export default function Document() {
   return (
     <Html>
-      <Page title="Inicio" description="teste">
+      {/* <Page title="Inicio" description="teste"> */}
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </Page>
+      {/* </Page> */}
     </Html>
   )
 }
