d0455b3cc0e1e7b8802f63c54413b53f7fe1dc66
Paulo Soares <paulosjunior7@gmail.com>
2022-12-05 02:10:15 -0300

    document

--- components/Page.js
+++ components/Page.js
@@ -1,18 +1,18 @@
-import { Head } from 'next/document'
+// import { Head } from 'next/document'
 
-function Page({ title, description, children }) {
-  return (
-    <div>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
-        <title>{title}</title>
-        <meta name="description" content={description}></meta>
-      </Head>
-      {children}
-    </div>
-  )
-}
+// function Page({ title, description, children }) {
+//   return (
+//     <div>
+//       <Head>
+//         <link rel="preconnect" href="https://fonts.googleapis.com" />
+//         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+//         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
+//         <title>{title}</title>
+//         <meta name="description" content={description}></meta>
+//       </Head>
+//       {children}
+//     </div>
+//   )
+// }
 
-export default Page;
+// export default Page;
